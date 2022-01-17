cd5ca9afc41aa51ab2be6d24eb29873e9871e172
Michał Płużek <michalpluzek@gmail.com>
2022-01-17 10:10:09 +0100

    feat: Add notes list component and add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: NotesListComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
