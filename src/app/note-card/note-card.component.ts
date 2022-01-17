49e6681352e70cd4a3a114a6c5207608c5ee6a53
Michał Płużek <michalpluzek@gmail.com>
2022-01-17 12:17:52 +0100

    fix: Truncator show/hide by JS

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
-  styleUrls: ['./note-card.component.scss']
+  styleUrls: ['./note-card.component.scss'],
 })
 export class NoteCardComponent implements OnInit {
+  @ViewChild('truncator', { static: true }) truncator: ElementRef<HTMLElement> =
+    {} as ElementRef<HTMLElement>;
+  @ViewChild('bodyText', { static: true }) bodyText: ElementRef<HTMLElement> =
+    {} as ElementRef<HTMLElement>;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {
-  }
+    // work out if is a text overflow and if not, then hide the truncator
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHight = parseInt(style.getPropertyValue('height'), 10);
 
+    if (this.bodyText.nativeElement.scrollHeight > viewableHight) {
+      // if there is a text overflow, show the fade out truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', ' block');
+    } else {
+      // else (there is a text overflow), hide the fade out trancator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
+  }
 }
