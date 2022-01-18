f09e840ff8823cfaf26198e50bf2db6749d50b78
Michał Płużek <michalpluzek@gmail.com>
2022-01-18 13:50:53 +0100

    feat: Add note list animation to improve user exp

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
-import { animate, style, transition, trigger } from '@angular/animations';
+import {
+  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-notes-list',
@@ -74,6 +81,21 @@ import { animate, style, transition, trigger } from '@angular/animations';
             paddingRight: 0,
             paddingLeft: 0,
           })
+        ),
+      ]),
+    ]),
+    trigger('listAnim', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [
+            style({
+              opacity: 0,
+              height: 0,
+            }),
+            stagger(100, [animate('0.2s ease')]),
+          ],
+          { optional: true }
         ),
       ]),
     ]),
