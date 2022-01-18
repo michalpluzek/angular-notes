24e6af1d87e6f444148d3221bd86afc623d72c53
Michał Płużek <michalpluzek@gmail.com>
2022-01-18 11:22:15 +0100

    feat: Add dynamic show notes

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Note } from 'src/app/shared/note.model';
+import { NotesService } from 'src/app/shared/notes.service';
 
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.scss']
+  styleUrls: ['./notes-list.component.scss'],
 })
 export class NotesListComponent implements OnInit {
+  notes: Note[] = new Array<Note>();
 
-  constructor() { }
+  constructor(private noteService: NotesService) {}
 
   ngOnInit(): void {
+    this.notes = this.noteService.getAll();
   }
-
 }
