c42fa0b1ab92f58aaee67579bf04404e6751c6f5
Michał Płużek <michalpluzek@gmail.com>
2022-01-18 10:56:00 +0100

    feat: Add simply form validation with styles

--- src/app/pages/note-details/note-details.component.ts
+++ src/app/pages/note-details/note-details.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Note } from 'src/app/shared/note.model';
 
 @Component({
   selector: 'app-note-details',
   templateUrl: './note-details.component.html',
-  styleUrls: ['./note-details.component.scss']
+  styleUrls: ['./note-details.component.scss'],
 })
 export class NoteDetailsComponent implements OnInit {
+  note: Note = {} as Note;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
-
 }
