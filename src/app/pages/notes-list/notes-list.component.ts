36adb25a756b36a0ee81d68eace5e044195c2ff7
Michał Płużek <michalpluzek@gmail.com>
2022-01-18 14:44:35 +0100

    feat: Add filtering notes

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -103,14 +103,58 @@ import {
 })
 export class NotesListComponent implements OnInit {
   notes: Note[] = new Array<Note>();
+  filteredNotes: Note[] = new Array<Note>();
 
   constructor(private noteService: NotesService) {}
 
   ngOnInit(): void {
     this.notes = this.noteService.getAll();
+    this.filteredNotes = this.notes;
   }
 
   deleteNote(id: number) {
     this.noteService.delete(id);
+  }
+
+  filter(query: string) {
+    query = query.toLowerCase().trim();
+
+    let allResults = new Array<Note>();
+    // split up search words
+    let terms: string[] = query.split(' ');
+    //remove duplicates
+    terms = this.removeDuplicates(terms);
+    // add relevant results to allResults array
+    terms.forEach((word) => {
+      let results: Note[] = this.revelantNotes(word);
+      allResults = [...allResults, ...results];
+    });
+
+    // allResults may contain duplicate notes,
+    // so they have to be removed
+    const uniqueResults = this.removeDuplicates(allResults);
+    this.filteredNotes = uniqueResults;
+  }
+
+  removeDuplicates(arr: any[]): any[] {
+    const uniqueResults: Set<any> = new Set<any>();
+
+    arr.forEach((item) => uniqueResults.add(item));
+
+    return [...uniqueResults];
+  }
+
+  revelantNotes(query: string): Note[] {
+    query = query.toLowerCase().trim();
+    const relevantNotes = this.notes.filter((note) => {
+      if (
+        note.body.toLowerCase().includes(query) ||
+        note.title.toLowerCase().includes(query)
+      )
+        return true;
+      return false;
+    });
+
+    return relevantNotes;
   }
 }
